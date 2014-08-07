b5c640d6aae0161ca134f7355329cc04d4f6bd5a
Jon Nagel <j.nagelxz@gmail.com>
2014-08-07 11:57:22 -0400

    Logging features now work. I just need to clean up the terminal junk recommit and it's all done.  The only thing left is to make the base directory partof the json object so that it can be moved to other machines, other than my work mac.  As for the logging, the this should work on windows machines, but I wouldn't guarantee it. I was designing this for *nix systems from the beginning.

--- manga-downloader.py
+++ manga-downloader.py
@@ -2,13 +2,26 @@ import os
 import time
 import json
 import zipfile
+import shutil
+import logging
+import logging.handlers
 from urllib import request as url
 from urllib import error as urlError
 
-base_path = '/Users/jonnagel/Documents/Personal Projects/manga-downloader/tmp/'
+base_path = '/Users/jonnagel/Documents/Personal Projects/manga-downloader/'
 base_url = 'http://s9.eatmanga.com/mangas/Manga-Scan/'
 # base_url = 'http://r1.goodmanga.net/images/manga/'
-data = json.loads(open(base_path + 'manga.json').read())
+data = json.loads(open(os.path.join(base_path, 'manga.json')).read())
+
+def set_logging():
+	serverlog = logging.FileHandler('/var/log/manga-downloader.log')
+	serverlog.setLevel(logging.INFO)
+	serverlog.setFormatter(logging.Formatter('%(asctime)s : %(levelname)s %(message)s'))
+
+	logger = logging.getLogger('manga-downloader-log')
+	logger.setLevel(logging.INFO)
+	logger.addHandler(serverlog)
+	return logger
 
 def ch_formatter(last_ch):
 	ch = ''
@@ -31,92 +44,115 @@ def pg_formatter(pg_num):
 		pg = str(pg_num)
 		
 	return pg
+	
+logger = set_logging()
+logger.info('Starting a new week\'s download')
+
+print (json.dumps(data, sort_keys=True,indent=3, separators=(',',':')))
+logger.info('Opening json')
 
 for manga in data['series']:
-	if not os.path.exists(base_path + manga['name']):
-		os.makedirs(base_path + manga['name'])
+	if not os.path.exists(os.path.join(base_path, 'tmp', manga['name'])):
+		os.makedirs(os.path.join(base_path, 'tmp', manga['name']))
+		logger.info('Creating place for ' + manga['name'] + ' to download')
+	if not os.path.exists(os.path.join(base_path, 'done', manga['name'])):	
+		os.makedirs(os.path.join(base_path, 'done', manga['name']))
+		logger.info('This (' + manga['name'] + ') must be new. Creating place for it to be stored...')
 
 	name = manga['name'].replace(' ', '-')
 #	name = manga['name'].replace(' ', '_').lower()
 	last_ch = manga['last']
-	print (name + '\t' + str(last_ch))
 	last_ch += 1
+	print (name + '\t' + str(last_ch))
+	logger.info('getting ready for ' + name + '\t' + str(last_ch))
 	
 	ch = ch_formatter(last_ch)
 	
-	print (ch)
 	url_no_pg = base_url + name + '/' + name + '-' + ch + '/'
 #	url_no_pg = base_url + name + '/' + str(last_ch) + '/'
-	print (url_no_pg)
 	
 	more_ch = 1
 	
 	while more_ch == 1:
-		if not os.path.exists(base_path + manga['name'] + '/' + ch):
-			os.makedirs(base_path + manga['name'] + '/' + ch)
-			
 		pg_num = 1
 		
 		try:
+			url.urlopen(url_no_pg + '01.jpg')
+			
+			if not os.path.exists(os.path.join(base_path, 'tmp', manga['name'], ch)):
+				os.makedirs(os.path.join(base_path, 'tmp', manga['name'], ch))
+				logger.info('creating place for the chapter')
+		
 			while pg_num != 0:
 				pg = pg_formatter(pg_num)
 #				pg = str(pg_num)
 			
 				try:
 #					print (url_no_pg + pg + '.jpg')
-					url.urlretrieve(url_no_pg + pg + '.jpg', base_path + manga['name'] + '/' + ch + '/' + pg+ '.jpg')
+					url.urlretrieve(url_no_pg + pg + '.jpg', os.path.join(base_path, 'tmp', manga['name'], ch, pg + '.jpg'))
 #					url.urlretrieve(url_no_pg + pg + '.jpg', base_path + manga['name'] + '/' + str(last_ch) + '/' + pg+ '.jpg')
 					print ('downloaded ' + pg + '.jpg')
+					logger.info('downloaded ' + pg + '.jpg')
 					pg_num += 1
-					time.sleep(15)
+					time.sleep(8)
 				except urlError.HTTPError:
 					print ('CHAPTER DONE')
+					logger.info('CHAPTER DONE')
 					pg_num = 0
-					time.sleep(60)
+					time.sleep(15)
 						
 			last_ch += 1
 			print (name + '\t' + str(last_ch))
+			logger.info('getting ready for ' + name + '\t' + str(last_ch))
+	
 			ch = ch_formatter(last_ch)
 #			print (ch)
 			url_no_pg = base_url + name + '/' + name + '-' + ch + '/'
 #			url_no_pg = base_url + name + '/' + str(last_ch) + '/'
-			print (url_no_pg)
-			url.urlopen(url_no_pg + '1.jpg')
 		
 		except urlError.HTTPError:
 			print ('no more chapters')
+			logger.info('Nothing more in this manga to download')
 			last_ch -= 1
 			more_ch = 0
-			time.sleep(90)
+			time.sleep(25)
 			
 	manga['last'] = last_ch
 	print (name + '\t' + str(last_ch))
 
 print ('done downloading')
+logger.info('Finished required downloads...')
 
-jsonfile = open(base_path + 'manga.json', 'w+')
+print (json.dumps(data, sort_keys=True,indent=3, separators=(',',':')))
+
+jsonfile = open(os.path.join(base_path, 'manga.json'), 'w+') 
 jsonfile.write(json.dumps(data, sort_keys=True,indent=3, separators=(',',':')))
 print ('json updated')
-				
-dirs = [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
-print (dirs)
+logger.info('Updating json')
+logger.info('...')
+logger.info('...')
+logger.info('converting mass downloads to comic chapters')
+
+dirs = [d for d in os.listdir(os.path.join(base_path, 'tmp')) if os.path.isdir(os.path.join(base_path, 'tmp', d))]
 
 for dir in dirs:
-	chapters = [c for c in os.listdir(base_path + dir + '/') if os.path.isdir(os.path.join(base_path + dir + '/', c))]
-	print (chapters)
+	chapters = [c for c in os.listdir(os.path.join(base_path, 'tmp', dir)) if os.path.isdir(os.path.join(base_path, 'tmp', dir, c))]
 	
 	for ch in chapters:
-		pgs = [p for p in os.listdir(base_path + dir + '/' + ch + '/') if os.path.isfile(os.path.join(base_path + dir + '/' + ch + '/', p))]
-		print (pgs)
-				
+		pgs = [p for p in os.listdir(os.path.join(base_path, 'tmp', dir, ch)) if os.path.isfile(os.path.join(base_path, 'tmp', dir, ch, p))]
+			
+		zip = zipfile.ZipFile(os.path.join(base_path, 'done', dir, ch + '.cbz'), 'w')	
+		logger.info('Creating Zip file: ' + os.path.join(base_path, 'done', dir, ch + '.cbz'))
 		for pg in pgs:
-			zip = zipfile.ZipFile(base_path + dir + '/' + ch + '.zip', 'w')
 			if len(pgs) != 0:
-				
-				print (os.path.join(base_path, dir, ch, pg))
-				zip.write(os.path.join(base_path, dir, ch, pg))
+				zip.write(os.path.join(base_path, 'tmp', dir, ch, pg))
 			else:
 				break
 				
-			zip.close()
+		zip.close()
+		logger.info('chapter done')
+		
+shutil.rmtree(os.path.join(base_path, 'tmp'))
+logger.info('Taking out the trash. Kicking it to the curb. Sending into the abyss of nothingness')
 print ('FINISHED')
+logger.info('FINISHED....Until next week!')
